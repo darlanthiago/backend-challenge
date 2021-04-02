ab4e08a2eaba6f081629ac9b91e7be8164fc5038
Darlan Thiago <tdarlan1@hotmail.com>
2021-04-01 22:22:21 -0300

    Add User and Auth Routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => '/user'], function () {
+
+    Route::post('/register', [UserController::class, 'store']);
+
+    Route::group(['middleware' => ['auth:sanctum']], function () {
+
+        Route::get('/me', [UserController::class, 'me']);
+
+        Route::delete('/{id}', [UserController::class, 'destroy']);
+    });
+});
+
+
+Route::group(['prefix' => 'auth'], function () {
+
+    Route::post('/login', [AuthController::class, 'store']);
+
+    Route::group(['middleware' => ['auth:sanctum']], function () {
+
+        Route::delete('/logout', [AuthController::class, 'destroy']);
+    });
 });
