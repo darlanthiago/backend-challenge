10e47228a0c7d09b70760f360875aa400ed41a49
Darlan Thiago <tdarlan1@hotmail.com>
2021-04-04 19:02:36 -0300

    Add Notification Status Event

---
+++ app/Events/NotificationTransaction.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Transaction;
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+
+class NotificationTransaction implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+
+    public $transaction;
+    public $user;
+
+
+    public function __construct(Transaction $transaction, User $user)
+    {
+        $this->transaction = $transaction;
+        $this->user = $user;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('send.transaction.status.' . $this->user->id);
+    }
+
+    public function broadcastWith()
+    {
+        return $this->transaction->toArray();
+    }
+}
